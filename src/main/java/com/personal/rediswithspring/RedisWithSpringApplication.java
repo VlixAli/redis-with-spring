6547224fa62ef86def1cfb5262513fd4d559798b
Ali Eldiasty <qali372@gmail.com>
2024-11-26 14:13:59 +0200

    initial commit

---
+++ src/main/java/com/personal/rediswithspring/RedisWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.personal.rediswithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisWithSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/personal/rediswithspring/RedisWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.rediswithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
